e30928d90a3d65ba7a9a97ddb8341f120cd08919
Alec Mac <alec@alecm.ac>
2022-05-21 12:27:27 -0700

    Initial Commit

---
+++ Hac News/Hac_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hac_NewsApp.swift
+//  Hac News
+//
+//  Created by Alec Mac on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Hac_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
