5f130afeceb6c96ee2ae1d314e921e4141c6cfb8
Alec Mac <alec@alecm.ac>
2022-05-22 21:55:32 -0700

    added nav link

---
+++ Hac News/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  Hac News
+//
+//  Created by Alec Mac on 5/22/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://google.com")
+        
+    }
+}
